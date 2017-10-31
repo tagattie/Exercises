d2f1222a934771762f6722e3f1cc02745ef8eeed
Hiroki Tagato <tagattie@gmail.com>
2017-10-31 19:49:54 +0900

    Add missing lines.

--- ProgrammingElixir/Chapter-5/first_steps/handle_open.exs
+++ ProgrammingElixir/Chapter-5/first_steps/handle_open.exs
@@ -2,3 +2,8 @@ handle_open = fn
   {:ok, file}  -> "First line: #{IO.read(file, :line)}"
   {_,   error} -> "Error: #{:file.format_error(error)}"
 end
+
+# 存在するファイルを開く
+IO.puts handle_open.(File.open("handle_open.exs"))
+# そして、存在しないファイルを開こうとする
+IO.puts handle_open.(File.open("nonexistent"))
