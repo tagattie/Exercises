07ada0b5358b870f8a11e7e6d21524914c76ece5
Hiroki Tagato <tagattie@gmail.com>
2017-11-07 19:25:43 +0900

    デフォルトパラメータ.

---
+++ ProgrammingElixir/Chapter-6/mm/default_params2.exs
@@ -0,0 +1,25 @@
+defmodule Params do
+
+  def func(p1, p2 \\ 123)
+
+  def func(p1, p2) when is_list(p1) do
+    "You said #{p2} with a list"
+  end
+
+  def func(p1, p2) do
+    "You passed in #{p1} and #{p2}"
+  end
+
+end
+
+IO.puts Params.func(99)           # You passed in 99 and 123
+IO.puts Params.func(99, "cat")    # You passed in 99 and cat
+IO.puts Params.func([99])         # You said 123 with a list
+IO.puts Params.func([99], "dog")  # You said dog with a list
+
+# iex(1)> c "default_params2.exs"
+# You passed in 99 and 123
+# You passed in 99 and cat
+# You said 123 with a list
+# You said dog with a list
+# [Params]
