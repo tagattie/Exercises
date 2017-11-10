716ead1641263683986b52793b355573b5e6045b
Hiroki Tagato <tagattie@gmail.com>
2017-11-10 11:32:16 +0900

    ヘッドとテイルを使ったリストの構築.

--- ProgrammingElixir/Chapter-7/lists/mylist1.exs
+++ ProgrammingElixir/Chapter-7/lists/mylist1.exs
@@ -1,6 +1,9 @@
 defmodule MyList do
   def len([]), do: 0
   def len([_head | tail]), do: 1 + len(tail)
+
+  def square([]), do: []
+  def square([head | tail]), do: [ head*head | square(tail) ]
 end
 
 # iex(3)> c "mylist1.exs"
@@ -15,3 +18,13 @@ end
 #
 # iex(5)> MyList.len(["cat", "dog"])
 # 2
+
+# iex(6)> c "mylist1.exs"
+# warning: redefining module MyList (current version defined in memory)
+#   mylist1.exs:1
+#
+# [MyList]
+# iex(7)> MyList.square []
+# []
+# iex(8)> MyList.square [4,5,6]
+# [16, 25, 36]
