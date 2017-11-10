054dbc25fd951052b6688539bd2ce5a9638e7731
Hiroki Tagato <tagattie@gmail.com>
2017-11-10 12:28:34 +0900

    map 関数の作成.

--- ProgrammingElixir/Chapter-7/lists/mylist1.exs
+++ ProgrammingElixir/Chapter-7/lists/mylist1.exs
@@ -7,6 +7,9 @@ defmodule MyList do
 
   def add_1([]), do: []
   def add_1([head | tail]), do: [ head+1 | add_1(tail) ]
+
+  def map([], _func), do: []
+  def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
 end
 
 # iex(3)> c "mylist1.exs"
@@ -41,3 +44,16 @@ end
 # [1001]
 # iex(11)> MyList.add_1 [4,6,8]
 # [5, 7, 9]
+
+# iex(1)> c "mylist1.exs"
+# [MyList]
+# iex(2)> MyList.map [1,2,3,4], fn (n) -> n*n end
+# [1, 4, 9, 16]
+# iex(3)> MyList.map [1,2,3,4], fn (n) -> n+1 end
+# [2, 3, 4, 5]
+# iex(4)> MyList.map [1,2,3,4], fn (n) -> n > 2 end
+# [false, false, true, true]
+# iex(5)> MyList.map [1,2,3,4], &(&1+1)
+# [2, 3, 4, 5]
+# iex(6)> MyList.map [1,2,3,4], &(&1 > 2)
+# [false, false, true, true]
