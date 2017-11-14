2e5b92fc2dd8271a54e93618587751f1996428d3
Hiroki Tagato <tagattie@gmail.com>
2017-11-14 16:33:52 +0900

    キーワードリスト.

---
+++ ProgrammingElixir/Chapter-8/maps/keywords.exs
@@ -0,0 +1,36 @@
+defmodule Canvas do
+
+  @defaults [ fg: "black", bg: "white", font: "Merriweather" ]
+
+  def draw_text(text, options \\ []) do
+    options = Keyword.merge(@defaults, options)
+    IO.puts "Drawing text #{inspect(text)}"
+    IO.puts "Foreground:  #{options[:fg]}"
+    IO.puts "Background:  #{Keyword.get(options, :bg)}"
+    IO.puts "Font:        #{Keyword.get(options, :font)}"
+    IO.puts "Pattern:     #{Keyword.get(options, :pattern, "solid")}"
+    IO.puts "Style:       #{inspect Keyword.get_values(options, :style)}"
+  end
+
+end
+
+Canvas.draw_text("hello", fg: "red", style: "italic", style: "bold")
+Canvas.draw_text("hello", style: "italic", style: "bold", pattern: "dotted")
+
+# iex(2)> c "keywords.exs"
+# warning: redefining module Canvas (current version defined in memory)
+#   keywords.exs:1
+#
+# Drawing text "hello"
+# Foreground:  red
+# Background:  white
+# Font:        Merriweather
+# Pattern:     solid
+# Style:       ["italic", "bold"]
+# Drawing text "hello"
+# Foreground:  black
+# Background:  white
+# Font:        Merriweather
+# Pattern:     dotted
+# Style:       ["italic", "bold"]
+# [Canvas]
