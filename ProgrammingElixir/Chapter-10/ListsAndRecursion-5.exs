3334e7b5524a28279bf0eefdbb7d7bc5a2ebf4ad
Hiroki Tagato <tagattie@gmail.com>
2017-11-15 16:51:36 +0900

    ListsAndRecursion-5 filter.

--- ProgrammingElixir/Chapter-10/ListsAndRecursion-5.exs
+++ ProgrammingElixir/Chapter-10/ListsAndRecursion-5.exs
@@ -12,4 +12,15 @@ defmodule MyEnum do
     [ func.(head) | each(tail, func) ]
   end
   def each([], _), do: []
+
+  # Enum.filter(list, func)
+  def filter(_list = [head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+  def filter([], _), do: []
+
 end
