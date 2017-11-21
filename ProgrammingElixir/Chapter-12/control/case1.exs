e4f8d28f10c1f629765719a3ffcd2909419e2b46
Hiroki Tagato <tagattie@gmail.com>
2017-11-21 18:03:46 +0900

    case.

---
+++ ProgrammingElixir/Chapter-12/control/case1.exs
@@ -0,0 +1,14 @@
+defmodule Users do
+  dave = %{ name: "Dave", state: "TX", likes: "programming" }
+
+  case dave do
+    %{state: some_state} = person ->
+      IO.puts "#{person.name} lives in #{some_state}"
+    _ ->
+      IO.puts "No matches"
+  end
+end
+
+# iex(11)> c "case1.exs"
+# Dave lives in TX
+# [Users]
