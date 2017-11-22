2cdc8b4860c146a18ef4cb088b4c64c2fca498a5
Hiroki Tagato <tagattie@gmail.com>
2017-11-22 17:12:16 +0900

    ライブラリをプロジェクトに追加する.

--- ProgrammingElixir/Chapter-13/issues/mix.exs
+++ ProgrammingElixir/Chapter-13/issues/mix.exs
@@ -6,6 +6,7 @@ defmodule Issues.Mixfile do
       app: :issues,
       version: "0.1.0",
       elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -23,6 +24,7 @@ defmodule Issues.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      { :httpoison, "~> 0.8" }
     ]
   end
 end
