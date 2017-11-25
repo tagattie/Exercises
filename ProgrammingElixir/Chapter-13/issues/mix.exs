11df74d4c7db41a99cc830fee4ca71e90e36ee0f
Hiroki Tagato <tagattie@gmail.com>
2017-11-25 22:34:04 +0900

    タスク: 実行可能なコマンドを作成.

--- ProgrammingElixir/Chapter-13/issues/mix.exs
+++ ProgrammingElixir/Chapter-13/issues/mix.exs
@@ -4,6 +4,7 @@ defmodule Issues.Mixfile do
   def project do
     [
       app: :issues,
+      escript: escript_config,
       version: "0.1.0",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
@@ -28,5 +29,9 @@ defmodule Issues.Mixfile do
       { :httpoison, "~> 0.8" },
       { :poison,    "~>1.5" }
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
