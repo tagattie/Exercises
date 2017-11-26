912eaeb639896074801f6ceb714295beb9351a45
Hiroki Tagato <tagattie@gmail.com>
2017-11-26 19:05:50 +0900

    NOAAObs: Initial import.

---
+++ ProgrammingElixir/Chapter-13/noaaobs/lib/noaaobs.ex
@@ -0,0 +1,18 @@
+defmodule Noaaobs do
+  @moduledoc """
+  Documentation for Noaaobs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Noaaobs.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ProgrammingElixir/Chapter-13/noaaobs/test/noaaobs_test.exs
@@ -0,0 +1,8 @@
+defmodule NoaaobsTest do
+  use ExUnit.Case
+  doctest Noaaobs
+
+  test "greets the world" do
+    assert Noaaobs.hello() == :world
+  end
+end
