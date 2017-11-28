1b90d6c0c11df1f7a6803768fd78227853aec752
Hiroki Tagato <tagattie@gmail.com>
2017-11-28 18:30:46 +0900

    Add a configuration for building an executable.

--- ProgrammingElixir/Chapter-13/noaaobs/mix.exs
+++ ProgrammingElixir/Chapter-13/noaaobs/mix.exs
@@ -4,6 +4,7 @@ defmodule NoaaObs.Mixfile do
   def project do
     [
       app: :noaaobs,
+      escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
@@ -24,5 +25,9 @@ defmodule NoaaObs.Mixfile do
     [
       { :httpoison, "~> 0.8"   }
     ]
+  end
+
+  defp escript_config do
+    [ main_module: NoaaObs ]
   end
 end
