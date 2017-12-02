b71f43a04ced1e66fb50e5347f4b1e71173a6088
Hiroki Tagato <tagattie@gmail.com>
2017-12-02 16:19:31 +0900

    サーバ実行のトラッキング.

--- Chapter-16/sequence/lib/sequence/server.ex
+++ Chapter-16/sequence/lib/sequence/server.ex
@@ -8,4 +8,9 @@ defmodule Sequence.Server do
   def handle_cast({:increment_number, delta}, current_number) do
     { :noreply, current_number + delta }
   end
+
+  def format_status(_reason, [ _pdict, state ]) do
+    [date: [{'State',
+             "My current state is '#{inspect state}', and I'm happy"}]]
+  end
 end
