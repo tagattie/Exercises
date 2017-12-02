b2913b93b224fc5c3d071cd3ac0e1c7f1768d32f
Hiroki Tagato <tagattie@gmail.com>
2017-12-02 18:00:55 +0900

    OTP-Servers-4.

--- Chapter-16/stack/lib/stack/server.ex
+++ Chapter-16/stack/lib/stack/server.ex
@@ -1,6 +1,18 @@
 defmodule Stack.Server do
   use GenServer
 
+  def start_link(list) do
+    GenServer.start_link __MODULE__, list, name: __MODULE__
+  end
+
+  def pop do
+    GenServer.call __MODULE__, :pop
+  end
+
+  def push(element) do
+    GenServer.cast __MODULE__, { :push, element }
+  end
+
   def handle_call(:initialize, _from, list) do
     { :reply, :ok, list }
   end
