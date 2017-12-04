46269215684973570e949764b2dc7fedfa087e77
Hiroki Tagato <tagattie@gmail.com>
2017-12-04 22:14:48 +0900

    サーバの状態のマイグレート.

--- Chapter-17/sequence/lib/sequence/server.ex
+++ Chapter-17/sequence/lib/sequence/server.ex
@@ -1,7 +1,12 @@
 defmodule Sequence.Server do
   use GenServer
+  require Logger
 
-  @vsn "0"
+  @vsn "1"
+
+  defmodule State do
+    defstruct current_number: 0, stash_pid: nil, delta: 1
+  end
 
   #####
   # 外部 API
@@ -23,23 +28,37 @@ defmodule Sequence.Server do
 
   def init(stash_pid) do
     current_number = Sequence.Stash.get_value stash_pid
-    { :ok, {current_number, stash_pid} }
+    { :ok, %State{current_number: current_number, stash_pid: stash_pid} }
   end
 
-  def handle_call(:next_number, _from, {current_number, stash_pid}) do
-    { :reply, current_number, {current_number+1, stash_pid} }
+  def handle_call(:next_number, _from, state) do
+    { :reply,
+      state.current_number,
+      %{ state | current_number: state.current_number + state.delta} }
   end
 
-  def handle_cast({:increment_number, delta}, {current_number, stash_pid}) do
-    { :noreply, {current_number + delta, stash_pid} }
+  def handle_cast({:increment_number, delta}, state) do
+    { :noreply,
+      %{ state | current_number: state.current_number + delta, delta: delta } }
   end
 
-  def terminate(_reason, {current_number, stash_pid}) do
-    Sequence.Stash.save_value stash_pid, current_number
+  def terminate(_reason, state) do
+    Sequence.Stash.save_value state.stash_pid, state.current_number
   end
 
   def format_status(_reason, [ _pdict, state ]) do
     [date: [{'State',
              "My current state is '#{inspect state}', and I'm happy"}]]
+  end
+
+  def code_change("0", old_state = { current_number, stash_pid }, _extra) do
+    new_state = %State{current_number: current_number,
+                       stash_pid: stash_pid,
+                       delta: 1
+                      }
+    Logger.info "Changing code from 0 to 1"
+    Logger.info inspect(old_state)
+    Logger.info inspect(new_state)
+    { :ok, new_state }
   end
 end
