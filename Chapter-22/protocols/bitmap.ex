f17674697a795ca8827f6c44210c8267e0f5ab84
Hiroki Tagato <tagattie@gmail.com>
2017-12-11 15:28:44 +0900

    Add an execution example.

--- Chapter-22/protocols/bitmap.ex
+++ Chapter-22/protocols/bitmap.ex
@@ -19,3 +19,14 @@ defmodule Bitmap do
     (value >>> bit) &&& 1
   end
 end
+
+# iex(1)> c "bitmap.ex"
+# [Bitmap]
+# iex(2)> b = %Bitmap{value: 5}
+# %Bitmap{value: 5}
+# iex(3)> Bitmap.fetch_bit(b, 2)
+# 1
+# iex(4)> Bitmap.fetch_bit(b, 1)
+# 0
+# iex(5)> Bitmap.fetch_bit(b, 0)
+# 1
